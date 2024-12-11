6730f3e56d1a7bc5711e1b060d53439d2fda410b
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-11 17:03:45 +0200

    Added a public folder and ejs engine

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 
 app.get("/", (request, response) => {
   response.send("Hello from sever");
