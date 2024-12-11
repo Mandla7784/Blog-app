a9c8983b867e120f5240ecc4ae96cb10fb48afd3
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-12-11 16:58:52 +0200

    Added a PORT

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (request, response) => {
+  response.send("Hello from sever");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
